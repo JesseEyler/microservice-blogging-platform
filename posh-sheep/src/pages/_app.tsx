a94035857119617ea95b3ffe8f406fd828d7f47d
JesseEyler <jesse.eyler11@gmail.com>
2023-04-05 12:08:35 -0400

    feat: article listing page
    Got rid of some example code given by create-next-app.
    Introduced a functioning article listing page using example code from the following Medium article:
    https://medium.com/technest/build-a-food-blog-with-next-js-mdx-tailwind-css-and-typescript-24380c1c6ed3
    Clicking any article link will bring up a 404 page, shall be fixed in next commit.

--- posh-sheep/src/pages/_app.tsx
+++ posh-sheep/src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+import type { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
---
+++ posh-sheep/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const SITE_URL = 'http://localhost:3000';
+export const SITE_NAME = 'A Really Sick Food Blog.';
+export const TWITTER_USERNAME = '@MySickFoodBlog';
